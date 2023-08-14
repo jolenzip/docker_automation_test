2c211b4c991e52a423013f7283bf4379cea4159c
Lenz <Jonas.Lenz@iproconsult.com>
2023-08-14 14:15:40 +0200

    add conda-forge, read online txt

--- test_example.py
+++ test_example.py
@@ -5,3 +5,15 @@ def inc(x):
 
 def test_answer():
     assert inc(4) == 5
+
+
+def read_txt(target_url):
+  import urllib2
+  data = urllib2.urlopen(target_url)
+  no = 0
+  for line in data: # files are iterable
+    no += 1
+  return no
+
+def test_lineno():
+  asser read_txt("https://www.w3.org/TR/2003/REC-PNG-20031110/iso_8859-1.txt") > 5
