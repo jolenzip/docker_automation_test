845c7da350c34da001a0bed4461587733398fba1
Lenz <Jonas.Lenz@iproconsult.com>
2023-08-14 14:52:51 +0200

    test gdal

--- test_example.py
+++ test_example.py
@@ -8,12 +8,20 @@ def test_answer():
 
 
 def read_txt(target_url):
-  import urllib.request
-  data = urllib.request.urlopen(target_url)
-  no = 0
-  for line in data: # files are iterable
-    no += 1
-  return no
+    import urllib.request
+    data = urllib.request.urlopen(target_url)
+    no = 0
+    for line in data: # files are iterable
+        no += 1
+    return no
 
 def test_lineno():
-  asser read_txt("https://www.w3.org/TR/2003/REC-PNG-20031110/iso_8859-1.txt") > 5
+    assert read_txt("https://www.w3.org/TR/2003/REC-PNG-20031110/iso_8859-1.txt") > 5
+
+
+def get_gdal():
+    from osgeo import gdal
+    return(gdal.__version__)
+
+def test_gdal():
+    assert not (get_gdal() == "")
